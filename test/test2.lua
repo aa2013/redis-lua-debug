926b3f9b5c502b816773eeeb20a1afe8b5bf722c
aa2013 <47207976+aa2013@users.noreply.github.com>
2026-03-18 14:28:51 +0800

    Update test2.lua

--- test/test2.lua
+++ test/test2.lua
@@ -1,2 +1,3 @@
-﻿redis.call('set',KEYS[1],ARGV[1])
-return redis.call('get',KEYS[1])
+redis.call('set',KEYS[1],ARGV[1])
+local json = cjson.encode({a=1,b=2})
+return redis.call('get',KEYS[1])..','..json
